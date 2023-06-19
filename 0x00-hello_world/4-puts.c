439ca88c8e09feb347561352eaf31d43f9acaded
Lincoln-Madaraka <madarakalincoln48@gmail.com>
2023-06-19 07:51:43 -0700

    My Commit on C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #include <stdio.h>
 /**
 *main-Entry point
@@ -6,7 +7,5 @@
 *Return :Always 0 (Success)
 */
 int main (void)
-{
-	puts("/"Programming is like building a multilingual puzzle");
-		return (0);
-}		
+{puts("\"Programming is like building a multilingual puzzle");
+	return(0);}		
