e8ea3b1437761e11231d85f3f6ef6f665bec230b
Lincoln-Madaraka <madarakalincoln48@gmail.com>
2023-06-19 09:29:08 -0700

    My commit number 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
 *main-Entry point
-*
-*Return :Always 0 (Success)
+*Return:Always 0 (Success)
 */
-int main (void)
-{puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);}		
+int main(void)
+{
+put("\"Programming is like building a multilingual puzzle");
+return (0);
+}
