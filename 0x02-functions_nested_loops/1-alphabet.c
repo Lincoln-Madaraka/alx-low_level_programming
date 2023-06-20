557e901892382518a402d88ba933c4d6bc08ad81
Lincoln-Madaraka <madarakalincoln48@gmail.com>
2023-06-20 11:13:09 -0700

    My functions nested loops commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_alphabet - print all alphabet in lower cse
+*/
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar('\n');
+	_putchar('\n');
+}
