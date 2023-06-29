f2671598a71044a8e8624d65864dc3a7dfc9fcdc
Lincoln-Madaraka <madarakalincoln48@gmail.com>
2023-06-29 16:25:36 -0700

    My commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes words in a string
+ * @s: string to be capitalized
+ * Return: A pointer to the changed string
+ */
+
+char *cap_string(char *s)
+{
+	int string_count;
+
+	string_count = 0;
+
+	while (s[string_count] != '\0')
+	{
+	if (s[0] >= 97 && s[0] <= 122)
+	{
+		s[0] = s[0] - 32;
+	}
+	if (s[string_count] == ' ' ||
+			s[string_count] == '\t' ||
+			s[string_count] == '\n' ||
+			s[string_count] == ',' ||
+			s[string_count] == ';' ||
+			s[string_count] == '.' ||
+			s[string_count] == '!' ||
+			s[string_count] == '?' ||
+			s[string_count] == '"' ||
+			s[string_count] == '(' ||
+			s[string_count] == ')' ||
+			s[string_count] == '{' ||
+			s[string_count] == '}')
+	{
+		if (s[string_count + 1] >= 97 && s[string_count + 1] <= 122)
+		{
+			s[string_count + 1] = s[string_count + 1] - 32;
+		}
+	}
+	string_count++;
+	}
+	return (s);
+}
